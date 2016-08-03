7f503c4d35fa802f31a971cdccee25216899f6b5
Satya Venkata Krishna Achanta <satyavenkatakrishnaachanta@Satyas-MBP.fios-router.home>
2016-08-03 13:28:38 -0400

    DataModel file added by satya

---
+++ gitTutorial/gitTutorial/DataModel.h
@@ -0,0 +1,13 @@
+//
+//  DataModel.h
+//  gitTutorial
+//
+//  Created by Satya Venkata Krishna Achanta on 8/3/16.
+//  Copyright © 2016 Muralidhar Kuruva. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DataModel : NSObject
+
+@end
---
+++ gitTutorial/gitTutorial/DataModel.m
@@ -0,0 +1,13 @@
+//
+//  DataModel.m
+//  gitTutorial
+//
+//  Created by Satya Venkata Krishna Achanta on 8/3/16.
+//  Copyright © 2016 Muralidhar Kuruva. All rights reserved.
+//
+
+#import "DataModel.h"
+
+@implementation DataModel
+
+@end
